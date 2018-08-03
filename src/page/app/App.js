a719ce80b9c62a1f430e64de0ef9ea76652ee8b5
Carlos Lenon <code@carloslenon.com>
2018-08-03 10:25:53 -0600

    reorganice seed add component for header

---
+++ src/page/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './../../common/components/header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <Header appName="Survey" />       
+      </div>
+    );
+  }
+}
+
+export default App;
